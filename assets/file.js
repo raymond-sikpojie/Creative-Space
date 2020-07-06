cb5443151ae457871e44c9d000dc4817fe61b8bb
raymond <raymondomon@yahoo.com>
2020-07-06 10:21:57 +0100

    minor changes

--- assets/file.js
+++ assets/file.js
@@ -1,46 +1,14 @@
+// Initialize animate on scroll
 AOS.init();
 
-// const form = document.querySelector(".form");
-// const userName = document.querySelector("#fname");
-// const userEmail = document.querySelector("#email");
-// const userNumber = document.querySelector("#number");
-// const userMessage = document.querySelector("#subject");
-
-// // Make post request
-// const postUser = async (data = {}) => {
-//   const response = await fetch("http://localhost:5000", {
-//     method: "POST",
-//     credentials: "same-origin",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   });
-//   try {
-//     const responseData = await response.json();
-//     if ((response.status = 200)) {
-//       alert(responseData.msg);
-//     }
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// };
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   // Collect form data
-//   let user = {
-//     name: userName.value,
-//     email: userEmail.value,
-//     number: userNumber.value,
-//     message: userMessage.value,
-//   };
-
-//   postUser(user);
-//   (userName.value = ""),
-//     (userEmail.value = ""),
-//     (userNumber.value = ""),
-//     (userMessage.value = "");
-// });
-
-// Initialize Animate on Scroll
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navbar").style.top = "0";
+  } else {
+    document.querySelector(".navbar").style.top = "-90px";
+  }
+  prevScrollpos = currentScrollPos;
+};
